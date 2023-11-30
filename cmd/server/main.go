eff0450a3bc908092e8e9b7584e167e685ea4a56
Konstantin <plusik.cs@gmail.com>
2023-11-30 14:30:55 +0500

    refactoring

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net"
+	"net/http"
+	"sync"
 
+	grpcValidator "github.com/grpc-ecosystem/go-grpc-middleware/validator"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/plusik10/note-service-api/config"
-	"github.com/plusik10/note-service-api/internal/app"
+	"github.com/plusik10/note-service-api/internal/app/note_v1"
+	noteRepository "github.com/plusik10/note-service-api/internal/repository/note"
+	"github.com/plusik10/note-service-api/internal/service/note"
+	desc "github.com/plusik10/note-service-api/pkg/note_v1"
+	"github.com/plusik10/note-service-api/pkg/postgres"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -12,5 +24,60 @@ func main() {
 	if err != nil {
 		log.Fatalf("config Err: %s", err.Error())
 	}
-	app.Run(cfg)
+	pg, err := postgres.New(cfg.PG.URL)
+	if err != nil {
+		log.Fatalf("err connection database %w", err)
+	}
+	defer pg.CloseConnect()
+
+	pgRepo := noteRepository.NewPostgresRepository(pg)
+	noteService := note.NewService(pgRepo)
+	note := note_v1.NewNote(noteService)
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		err = startHTTP(cfg)
+	}()
+	go func() {
+		defer wg.Done()
+		startGRPC(cfg.GRPC.Port, note)
+	}()
+
+	wg.Wait()
+}
+
+func startHTTP(cfg *config.Config) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := desc.RegisterNoteV1HandlerFromEndpoint(ctx, mux, cfg.GRPC.Port, opts)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("HTTPServer is running on : %s\n", cfg.HTTP.Port)
+
+	return http.ListenAndServe(cfg.HTTP.Port, mux)
+}
+
+func startGRPC(grpcPort string, server desc.NoteV1Server) error {
+	list, err := net.Listen("tcp", ":50052")
+	if err != nil {
+		log.Fatalf("failed to mapping port: %s", err.Error())
+	}
+
+	s := grpc.NewServer(grpc.UnaryInterceptor(grpcValidator.UnaryServerInterceptor()))
+	desc.RegisterNoteV1Server(s, server)
+	fmt.Printf("GRPCServer is running on : %s\n", grpcPort)
+
+	if err = s.Serve(list); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
 }
